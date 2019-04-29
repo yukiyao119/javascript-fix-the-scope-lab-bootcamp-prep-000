6269610328ad8b90c1af00933cd18b078450cca5
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 03:29:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  var animal = 'cat' //<-- declare a local varialble animal
+  var animal = 'cat' //<-- declare local varialble animal
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
